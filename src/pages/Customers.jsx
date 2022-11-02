b0c0f233c54f83136f4351cc987590c327d2313d
William Mosquera <wjmmk3@gmail.com>
2022-11-02 15:36:00 -0500

    Calendar-Page Finished

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -6,7 +6,7 @@ import { Header } from '../components';
 export const Customers = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Customers" />
+      <Header category="Pages" title="Customers" />
       <GridComponent
         dataSource={customersData}
         toolbar={['Delete']}
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -6,7 +6,7 @@ import { Header } from '../components';
 export const Orders = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Orders" />
+      <Header category="Pages" title="Orders" />
       <GridComponent
         id="gridComp"
         dataSource={ordersData}
