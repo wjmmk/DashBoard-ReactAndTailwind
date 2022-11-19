489a4f28bc676bd142593f018f012035a0544c11
William Mosquera <wjmmk3@gmail.com>
2022-11-19 10:22:12 -0500

    Images added

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -6,7 +6,7 @@ import { Header } from '../components';
 export const Employees = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Pages" title="Employess" />
+      <Header category="Pages" title="Employees" />
       <GridComponent
         dataSource={employeesData}
         toolbar={['Search']}
